c6886072e58cb90b802d5c6d6ee52d97b2b36509
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-27 18:25:55 -0500

    about page text and styling added

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,10 @@ export default class About extends Component {
       <div className='app'>
         <Header />
         <Navbar/>
-        <h1>I am the about page</h1>
+        
+        <div className="about">
+        After a thirty year career at the Lincoln parish highway department I am selling things to add some beauty to your home.
+        </div>
       </div>
     );
   }
