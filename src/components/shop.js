917bbb8838210b788e43bfd3c4e3e0adde53f954
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-27 20:55:29 -0500

    started adding images to shop

--- src/components/shop.js
+++ src/components/shop.js
@@ -12,7 +12,16 @@ export default class Shop extends Component {
 
      
       <div className="shop">
-        
+        <div className="designs">
+          <img src ='./assets/images/designs/edit1.png'/>
+        </div>
+        <div className="designs">
+          <img src ='./assets/images/designs/welcome.jpg'/>
+        </div>
+        <div className="designs">
+          <img src ='./assets/images/designs/patriotic.jpg'/>
+        </div>
+     
       </div>
       </div>
       
