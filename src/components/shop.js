da44b47a59a6518fbb3690e1e46d2d5a17911df4
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-08-30 13:44:10 -0500

    got card flipping to work on shop and made colors varibles.

--- src/components/shop.js
+++ src/components/shop.js
@@ -10,13 +10,26 @@ export default class Shop extends Component {
      <Header />
        <Navbar/>
       <div className="shop">
+        
         <div className='shop-item__front'>
           <div className="top">
             <div className="designs">
               <img src ='./assets/images/designs/secondpass.jpg'/>
             </div>
+            </div>
+            {/* <div className='shop-item__front'>
+          <div className="top">
             <div className="designs">
-                <img src ='./assets/images/designs/welcome.jpg'/>
+              <img src ='./assets/images/designs/welcome.jpg'/>
+            </div>
+            </div>
+            </div> */}
+            {/* <div className="shop">
+              <div className='shop-item__front'>
+                  <div className="designs">
+                    <img src ='./assets/images/designs/welcome.jpg'/>
+                  </div>
+              </div>
             </div>
             <div className="designs">
                 <img src ='./assets/images/designs/patriotic.jpg'/>
@@ -32,17 +45,28 @@ export default class Shop extends Component {
             <div className="designs">
                 <img src ='./assets/images/designs/sunflower.jpg'/>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className='shop-item__back'>
-          I am the back
+          <div className="shop-item__back__title">
+            Test title
+          </div>
+          <div className="shop-item__back__description">
+            Test description
+          </div>
+          <div className="shop-item__back__price">
+            $3.50
+          </div>
+          <div className="shop-item__back__quantity">
+            2
+          </div>
+          <div className="shop-item__back__add-to-cart">
+            Add to Cart
+          </div>
         </div>
-        {/* <div className="shop-item__title">
-          Test title
-        </div> */}
        </div>
-   
-    </div>
+   </div>
+    
      );   
   }
 }
