cd67c4112a0451754c8e205310b300931cde1177
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-10-10 15:45:44 -0500

    trying to get cart of render.

--- src/components/shop/Cart.js
+++ src/components/shop/Cart.js
@@ -1,95 +1,51 @@
-import React, { Component } from 'react';
-import Item from './Item';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import Item from "./Item";
+import { connect } from "react-redux";
 
-import Header from '../headernavbar/header';
-import Navbar from '../headernavbar/navbar';
- 
-// class Cart extends Component
-// {
-//     render() {
- 
-//         let total = 0;
- 
-//         this.props.cart.map(item => total += item.product.price * item.quantity);
- 
-//         const cart  = this.props.cart.length > 0?(
-//             <div>
-//                 <div className="panel-body">
-//                     {
-//                         this.props.cart.map(item => {
-//                             return (
-//                                 <div key={item.product.id}>
-//                                     <Item item={item} />
-//                                     <hr />
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
-//                 <div className="panel-footer">
-//                     <div className="row text-center">
-//                         <div className="col-xs-11">
-//                             <h4 className="text-right">Total <strong>${total.toFixed(3)}</strong></h4>
-//                         </div>
- 
-//                     </div>
-//                 </div>
-//             </div>
- 
-//         ) : (
-//             <div className="panel-body">
-//                 <p>Cart is empty</p>
-//             </div>
-//         )
- 
-//         return (
-//                 <div className="container">
-//                      <Header />
-//                     <Navbar/>
-//                     <div className="row">
-//                         <div className="col-md-12 col-xs-12">
-//                             <div className="panel panel-info">
-//                                 <div className="panel-heading">
-//                                     <div className="panel-title">
-//                                         <div className="row">
-//                                             <div className="col-xs-6">
-//                                                 <h5><span className="glyphicon glyphicon-shopping-cart"></span> My Shopping Cart</h5>
-//                                             </div>
- 
-//                                         </div>
-//                                     </div>
-//                                 </div>
- 
-//                                 { cart }
- 
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//         )
-//     }
-// }
- 
-// const mapStateToProps = (state) => {
- 
-//   return {
-//       cart: state.cart
-//   }
-// };
- 
-// export default connect(mapStateToProps)(Cart);
+import Header from "../headernavbar/header";
+import Navbar from "../headernavbar/navbar";
 
 export default class Cart extends Component {
-    render() {
-      return (
-        <div className='app'>
-          <Header />
-          <Navbar/>
-          <div className='Cart'>
-             
+  render() {
+    let total = 0;
+    this.props.cart.map(
+      (item) => (total += item.product.price * item.quantity)
+    );
+
+    const cart =
+      this.props.cart.length > 0 ? (
+        <div>
+          <div className="panel-body">
+            {this.props.cart.map((item) => {
+              return (
+                <div key={item.product.id}>
+                  <Item item={item} />
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+          <div className="panel-footer">
+            <div className="row text-center">
+              <div className="col-xs-11">
+                <h4 className="text-right">
+                  Total <strong>${total.toFixed(3)}</strong>
+                </h4>
+              </div>
+            </div>
           </div>
         </div>
+      ) : (
+        <div className="panel-body">
+          <p>Cart is empty</p>
+        </div>
       );
-    }
+    return (
+      <div className="app">
+        <Header />
+        <Navbar />
+        <div className="Cart"></div>
+      </div>
+    );
   }
+}
