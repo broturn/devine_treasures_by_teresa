8a20b24a57007c696c3379fbb5e0e2ee54d7a5f5
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-10-31 18:23:27 -0500

    trying to get cart to work.

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,3 +1,4 @@
+export const ADD_TO_CART = "ADD_TO_CART"
 export const addToCart = (product) => {
  
     return {
--- src/components/shop/shopCard.js
+++ src/components/shop/shopCard.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from "react-redux"
+
+import { addToCart } from "../../actions/cartActions";
 
 const ShopCard = (props) => {
+  const dispatch = useDispatch()
 
     useEffect( () => {
         
@@ -21,7 +25,9 @@ const ShopCard = (props) => {
             <div className="shop-item__back__description">{props.product.description}</div>
             <div className="shop-item__back__price">${props.product.price}</div>
             <div className="shop-item__back__quantity" quantity={1} />
-            <div className="shop-item__back__add-to-cart">Add to Cart</div>
+            <button onClick={() => dispatch(addToCart(props.id))}>
+              Add To Cart
+            </button>
           </div>
         </div>
     );
