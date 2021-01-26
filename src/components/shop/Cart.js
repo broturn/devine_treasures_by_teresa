2e670a3109bf9c5597227a9ecf6a5e9646987c42
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-01-25 20:18:03 -0600

    next pass at getting the cart to work.

--- src/components/shop/Cart.js
+++ src/components/shop/Cart.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import Item from './Item';
-import { connect } from 'react-redux';
-import { useStateValue } from "../../reducers/StateProvider"
+// import React, { Component } from 'react';
+// import Item from './Item';
+// import { connect } from 'react-redux';
+// import { useStateValue } from "../../reducers/StateProvider"
 
-import Header from '../headernavbar/header';
-import Navbar from '../headernavbar/navbar';
+// import Header from '../headernavbar/header';
+// import Navbar from '../headernavbar/navbar';
 
-  function Checkout() {
-    const [{ basket, user }, dispatch] = useStateValue();
-    return (
-      <div className='app'>
-          <Header />
-          <Navbar/>
-          <div className='Cart'>
-          </div>
-        </div>
-    )
-  }
-      // this.props.cart.map(item => total += item.product.price * item.quantity);
+//   function Checkout() {
+//     const [{ basket, user }, dispatch] = useStateValue();
+//     return (
+//       <div className='app'>
+//           <Header />
+//           <Navbar/>
+//           <div className='Cart'>
+//           </div>
+//         </div>
+//     )
+//   }
+//       // this.props.cart.map(item => total += item.product.price * item.quantity);
       
         
 
     
-export default Cart;
+// export default Cart;
--- src/components/shop/Item.js
+++ src/components/shop/Item.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { useStateValue } from "../../reducers/StateProvider"
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import { useStateValue } from "../../reducers/StateProvider"
 
-function Item({ id, title, image, price, rating}) {
-  const [{ cart }, dispatch] = useStateValue();
+// function Item({ id, title, image, price, rating}) {
+//   const [{ cart }, dispatch] = useStateValue();
 
-  const addToCart = () => {
-    dispatch({
-      type: 'ADD_TO_CART',
-      item: {
-        id: id,
-        title: title,
-        description: description,
-        price: price,
-        imageUrl: imageUrl,
-      },
-    })
-  }
+//   const addToCart = () => {
+//     dispatch({
+//       type: 'ADD_TO_CART',
+//       item: {
+//         id: id,
+//         title: title,
+//         description: description,
+//         price: price,
+//         imageUrl: imageUrl,
+//       },
+//     })
+//   }
 
 
-return (
-  <div className="item">
-    <div className="item_info">
-      <p>{title}</p>
-    </div>
-  </div>
-)
-}
-export default Item;
+// return (
+//   <div className="item">
+//     <div className="item_info">
+//       <p>{title}</p>
+//     </div>
+//   </div>
+// )
+// }
+// export default Item;
